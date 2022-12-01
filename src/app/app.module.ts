c8cee88697ce8f0555c34f662478cb032d43c1cb
lumontesmeli <ext_lumontes@mercadolibre.com.co>
2022-12-01 12:05:37 -0500

    Se añade quienes somos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
+import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
+import { LegalComponent } from './pages/legal/legal.component';
+import { ContactenosComponent } from './pages/contactenos/contactenos.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: MainComponent },
+  { path: 'quienessomos', component: QuienesSomosComponent },
+  { path: 'legal', component: LegalComponent },
+  { path: 'contactenos', component: ContactenosComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    QuienesSomosComponent,
+    LegalComponent,
+    ContactenosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
